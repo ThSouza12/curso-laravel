6dff99cd31b2b906df8cd078f9b0c38505fa67f5
Th Souza <you@example.com>
2021-01-07 19:44:03 -0300

    atual

---
+++ app/Http/Controllers/AdminTesteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminTesteController extends Controller
+{
+    public function teste() 
+    {
+        return 'Teste Controller';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,79 @@
 <?php
 
+Route::get('/login', function () {
+    return 'Login';
+})->name('login');
 
 
+Route::middleware([])->group(function () {
+    
+    Route::prefix('admin')->group(function () {
+        
+        Route::get('/dashboard', 'AdminTesteController@teste');
+    
+        Route::get('/financeiro', 'AdminTesteController@teste');
+    
+        Route::get('/produtos','AdminTesteController@teste' );
+
+        Route::get('/','AdminTesteController@teste' );
+    });
+});
+
+
+
+
+Route::get('redirect3', function () {
+    return redirect()->route('url.name');
+});
+
+// route('url.name');
+
+Route::get('/nome-url', function () {
+    return 'hey hey hey';
+})->name('url.name');
+
+Route::view('/view', 'welcome', ['id' => 'teste']);
+
+Route::redirect('/redirect', '/redirect2' );
+
+// Route::get('redirect1', function () {
+//     return redirect('/redirect2');
+// });
+
+Route::get('redirect2', function () {
+    return 'redirect 02';
+});
+
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "produto(s) {$idProduct}";
+});
+
+Route::get('/categoria/{flag}/posts', function ($flag) {
+    return "posts da categoria:{$flag}";
+});
+
+Route::get('/categorias/{flag}', function ($prm1) {
+    return "produtos da categoria:{$prm1}";
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'Match';
+});
+
+Route::any('/any', function () {
+    return 'any';
+});    
+Route::post('/register', function () {
+        return '';
+    });
+
+Route::get('/empresa', function () {
+    return view ('site.contact');
+});
+   
+Route::get('/contato', function () {
+    return 'Contato';
+});
 
 Route::get('/', function () {
     return view('welcome');
